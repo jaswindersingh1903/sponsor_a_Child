b26ef8b2a0820c3b1ae9e5e1272eff9048a0e71e
Jaswinder Singh <32540681+jaswindersingh1903@users.noreply.github.com>
2022-12-07 00:24:27 -0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = 
+{
+  firebase: {
+    projectId: 'gsefdev',
+    appId: '1:266868016514:web:70cdc718a83269a92dfb9b',
+    databaseURL: 'https://gsefdev-default-rtdb.firebaseio.com',
+    storageBucket: 'gsefdev.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBEzf9Kr1R4Bh_xxdNuUWmSfd6y95Kj1_o',
+    authDomain: 'gsefdev.firebaseapp.com',
+    messagingSenderId: '266868016514',
+  },
+  production: true,
+  apiBaseUrl: 'http://127.0.0.1:8000/api/V1',
+};
